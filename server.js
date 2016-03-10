b05179d915f9a8d56c8e40a2948236f05b121cc5
Alemneh <Alemneh@hotmail.com>
2016-03-10 11:58:24 -0800

    POST and GET functioning and pssing tests

--- server.js
+++ server.js
@@ -2,46 +2,42 @@
 
 const http = require('http');
 const fs = require('fs');
-
-var data;
+var counter = 0;
 
 http.createServer((req, res) => {
+
   if(req.url === '/messages' && req.method === 'GET') {
     res.writeHead(200, {'content-type': 'text/html'});
     fs.readdir('data', (err, files) => {
-      console.log(files);
+      res.write('You have ' + files.length + ' new Messages\n');
       if(err) throw err;
       files.forEach( (file) => {
-        console.log('In forEach '+file);
-        console.log(typeof file);
         fs.readFile('data/' + file, 'utf8', (err, data) => {
           if(err) throw err;
-          console.log(JSON.parse(data).message);
-          res.write(JSON.parse(data).message);
+          res.write(JSON.parse(data).message + '\n');
+          counter++;
+          if(counter == files.length){
+            res.end();
+          }
+        }); // end readFile
+      });// end forEach
+    });// end readdir
+  }// end GET
 
-        });
-
-      });
-      res.end();
-    });
-
-  }
   if(req.url === '/messages' && req.method === 'POST') {
     req.on('data', (data) => {
-      console.log(JSON.parse(data));
-      fs.readdir('files', (err, files) => {
+      fs.readdir('data', (err, files) => {
         if(err) throw err;
-        console.log(files);
         var num = files.length;
         fs.writeFile('data/message' + num + '.json', data, (err) => {
           if(err) throw err;
-          console.log('File message'+num+'.json was saved!');
-        });
-      });
-      res.writeHead(200, {'content-type': 'text/html'});
-      return res.end();
-    });
-  }
+          res.writeHead(200, {'content-type': 'text/html'});
+          res.write('File message'+num+'.json was saved!');
+          return res.end();
+        });// end writeFile
+      });// end on readdir
+    });// end on data
+  }// end POST
 
 }).listen(3000, () => {
   console.log('Server running on port 3000');
